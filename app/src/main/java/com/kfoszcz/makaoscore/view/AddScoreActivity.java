7ad99c9953a30781156a20c248b5453390ce24c7
Krzysztof Foszcz <krzysztof.foszcz@gmail.com>
2018-03-10 17:27:32 +0100

    Added layouts for AddScoreView

---
+++ app/src/main/java/com/kfoszcz/makaoscore/view/AddScoreActivity.java
@@ -0,0 +1,15 @@
+package com.kfoszcz.makaoscore.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kfoszcz.makaoscore.R;
+
+public class AddScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_score);
+    }
+}
